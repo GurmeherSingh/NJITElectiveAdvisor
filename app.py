7282389f190e6ea3c9f9777b29c0c51d09325d85
Gurmeher Singh <gurumehersingh1706@gmail.com>
2025-09-04 02:51:21 -0400

    Fix Railway deployment configuration

--- app.py
+++ app.py
@@ -397,4 +397,5 @@ def saved_courses_page():
     return render_template('saved_courses.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
